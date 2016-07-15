aae67c15a699dbfb3de6b3b5c91e1947732d459b
Tyler <shed412@gmail.com>
2016-07-15 01:58:52 -0400

    Made it an infinite loop and gives the place. 52 is my favorite.

--- SquareAndTriangle.java
+++ SquareAndTriangle.java
@@ -4,19 +4,20 @@ class SquareAndTriangle
   {
     int both = 0;
     int n = 2;
+    boolean foundOne;
     System.out.println("Well 0 and 1 are obvious answers! But hopefully there are more...");
-      while(both < 10){
-        for(int m=1; m<=(n*n); m++){
-          boolean foundOne = (n*n == (m*(m+1))/2);
+      while(true){
+        for(int m=n; m<=(n*n); m++){
+          foundOne = (n*n == (m*(m+1))/2);
           if (foundOne){
             System.out.println("Matt, "+(n*n)+" is one!");
             System.out.println("m: "+m);
             System.out.println("n: "+n);
             both++;
+            System.out.println("Place: "+both);
           }//if
         }//for
         n++;
       }//while
-      System.out.println("And there are the first 10 that aren't 0, and 1!");
   }//main
 }//class
